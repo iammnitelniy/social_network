919cfa5761a4712e2f78dbb9a0b814584bd93962
abc <abc@example.com>
2023-03-11 17:11:49 +0600

    add

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import classes from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 
 type DialogsPropsType = any;
@@ -7,9 +8,41 @@ type DialogsPropsType = any;
 
 export const Dialogs = (props: DialogsPropsType) => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/1">Ilia</NavLink>
+
+                </div>
+                <div className={classes.dialog + " " + classes.active}>
+                    <NavLink to="/dialogs/2">Regina</NavLink>
+
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/3">Kyle</NavLink>
+
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/4">Stan</NavLink>
+
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/5">Cartman</NavLink>
+
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/6">Kenny</NavLink>
+
+                </div>
+
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>They</div>
+                <div className={classes.message}>have killed</div>
+                <div className={classes.message}>Kenny</div>
+            </div>
         </div>
     )
+
 }
 
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -10,14 +10,16 @@ export function Nav(props: NavPropsType) {
     return (
         <nav className={classes.nav}>
 
-            <div className={`${classes.item} ${classes.active}`}><NavLink to="/profile">{props.names[0]}</NavLink></div>
-            <div className={classes.item}><NavLink to="/dialogs">{props.names[1]}</NavLink></div>
-                <div className={classes.item}><NavLink to="/news">{props.names[2]}</NavLink></div>
-                <div className={classes.item}><NavLink to="/music">{props.names[3]}</NavLink></div>
-                <div className={classes.item}><NavLink to="/settings">{props.names[4]}</NavLink></div>
+            <div className={classes.item}><NavLink to="/profile" activeClassName={classes.active}>{props.names[0]}</NavLink></div>
+            <div className={classes.item}><NavLink to="/dialogs" activeClassName={classes.active}>{props.names[1]}</NavLink></div>
+                <div className={classes.item}><NavLink to="/news" activeClassName={classes.active}>{props.names[2]}</NavLink></div>
+                <div className={classes.item}><NavLink to="/music" activeClassName={classes.active}>{props.names[3]}</NavLink></div>
+                <div className={classes.item}><NavLink to="/settings" activeClassName={classes.active}>{props.names[4]}</NavLink></div>
 
 
 
         </nav>
     )
+
+/*    {`${classes.item} ${classes.active}`}*/
 }
