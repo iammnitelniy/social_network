c067cb4e29a07a45ab516b6c0f18ac7bd64a2324
abc <abc@example.com>
2023-03-19 05:07:54 +0600

    add

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -10,9 +10,16 @@ type DialogItemPropsType = {
 
 }
 type MessagePropsType = {
-    content: string;
+    message: string;
 }
-
+type DialogItemType = {
+    id: number
+    name: string
+}
+type MessageItemType= {
+    id: number
+    message: string
+}
 
 const DialogItem = (props: DialogItemPropsType) => {
     return (
@@ -24,27 +31,69 @@ const DialogItem = (props: DialogItemPropsType) => {
 
     )
 }
+
+
 const Message = (props: MessagePropsType) => {
     return (
 
-        <div className={classes.message}>{props.content}</div>
+        <div className={classes.message}>{props.message}</div>
 
     )
 }
 
 
 export const Dialogs = (props: DialogsPropsType) => {
+    let dialogsData: Array<DialogItemType> = [
+        {
+            id: 1,
+            name: "Ilia",
+        },
+        {
+            id: 2,
+            name: "Regina",
+        },
+        {
+            id: 3,
+            name: "Kyle",
+        },
+        {
+            id: 4,
+            name: "Stan",
+        },
+        {
+            id: 5,
+            name: "Cartman",
+        },
+        {
+            id: 6,
+            name: "Kenny",
+        }
+
+
+    ]
+    let messagesData: Array<MessageItemType> = [
+        {
+            id: 1,
+            message: "They",
+        },
+        {
+            id: 2,
+            message: "killed",
+        },
+        {
+            id: 3,
+            message: "Kenny",
+        }
+
+
+    ]
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
                 <div className={classes.dialog}>
 
-                    <DialogItem name={"Ilia"} id={1}/>
-                    <DialogItem name={"Regina"} id={2}/>
-                    <DialogItem name={"Kyle"} id={3}/>
-                    <DialogItem name={"Stan"} id={4}/>
-                    <DialogItem name={"Cartman"} id={5}/>
-                    <DialogItem name={"Kenny"} id={6}/>
+                    {dialogsData.map((el)=> <DialogItem key={el.id} id={el.id} name={el.name} />)}
+
 
                 </div>
                 <div className={classes.dialog}>
@@ -53,9 +102,8 @@ export const Dialogs = (props: DialogsPropsType) => {
 
             </div>
             <div className={classes.messages}>
-                <Message content={"They"} />
-                <Message content={"killed"} />
-                <Message content={"Kenny"} />
+                {messagesData.map((el)=> <DialogItem key={el.id} id={el.id} name={el.message} />)}
+
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,8 +3,43 @@ import classes from './MyPosts.module.css'
 import {Post} from "./Post/Post";
 
 type MyPostsPropsType = any;
+type PostItemType= {
+    id: number
+    content: string
+    countLikes: number
+    countDislikes: number
+}
 
 export function MyPosts(props: MyPostsPropsType) {
+    let postData: Array<PostItemType> = [
+        {
+            id: 1,
+            content: "bow",
+            countLikes: 12,
+            countDislikes: 9
+        },
+        {
+            id: 2,
+            content: "wow",
+            countLikes: 5,
+            countDislikes: 1
+        },
+        {
+            id: 3,
+            content: "get",
+            countLikes: 7,
+            countDislikes: 3
+        },
+        {
+            id: 4,
+            content: "down",
+            countLikes: 9,
+            countDislikes: 0
+        }
+
+
+
+    ]
     return (
         <div className={classes.postsBlock}>
             <div>
@@ -21,12 +56,8 @@ export function MyPosts(props: MyPostsPropsType) {
             </div>
 
             <div className={classes.posts}>
-
-                <Post content={"bow"} countLikes={12} countDislikes={0}/>
-                <Post content={"wow"} countLikes={16} countDislikes={7}/>
-                <Post content={"get"} countLikes={2} countDislikes={5}/>
-                <Post content={"down"} countLikes={9} countDislikes={3}/>
-
+                {postData.map((el, index) => <Post key={index} content={el.content} countLikes={el.countLikes}
+                                            countDislikes={el.countDislikes}/>)}
 
             </div>
         </div>
