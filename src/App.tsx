3b3cd44628fbd0e7238e3781e07103a5e7166a38
ILIABARABANOV <ilianardan@gmail.com>
2023-11-07 17:19:41 +0300

    feature: lazy loading for dialogs and profile

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,7 @@ import './App.css';
 import {Nav} from "./components/Nav/Nav";
 import {BrowserRouter, Route} from "react-router-dom";
 import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
-import {Login} from "./components/Login/Login";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import LoginContainer from "./components/Login/LoginContainer";
 import {AppStateType, store} from "./redux/redux-store";
 import {connect, Provider} from "react-redux";
@@ -16,6 +13,10 @@ import {News} from "./components/News/News";
 import {Music} from "./components/Music/Music";
 import {Settings} from "./components/Settings/Settings";
 import {compose} from "redux";
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'))
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'))
+
+
 
 type AppPropsType = {
 
@@ -28,7 +29,7 @@ export type DialogItemType = {
     id: number
     name: string
 }
-export type MessageItemType= {
+export type MessageItemType = {
     id: number
     message: string
 
@@ -37,20 +38,21 @@ export type MessageItemType= {
 
 
 
-const App = (props:AppPropsType) => {
+const App = (props: AppPropsType) => {
     const navbarNames = ["Profile", "Messages", "News", "Music", "Settings", "Users", 'Login']
     useEffect(() => {
         console.log(props.isInitialized)
-        props.initializeApp()}, [])
+        props.initializeApp()
+    }, [])
 
-if(!props.isInitialized) {
-    return <Preloader />
-}
+    if (!props.isInitialized) {
+        return <Preloader/>
+    }
     console.log(props.isInitialized)
     return (
         <BrowserRouter>
             <div className='app-wrapper'>
-                <HeaderContainer />
+                <HeaderContainer/>
                 <Nav
                     names={navbarNames}
                 />
@@ -64,14 +66,14 @@ if(!props.isInitialized) {
 
                     <Route path='/profile/:userId?'
                            render={() =>
-                    <ProfileContainer/>}/>
+                               <ProfileContainer/>}/>
 
-                    <Route path='/news' render={()=><News/>}/>
-                    <Route path='/music' component={()=><Music/>}/>
-                    <Route path='/settings' component={()=><Settings/>}/>
+                    <Route path='/news' render={() => <News/>}/>
+                    <Route path='/music' component={() => <Music/>}/>
+                    <Route path='/settings' component={() => <Settings/>}/>
 
-                    <Route path={'/users'} render={()=><UsersContainer/>}/>
-                    <Route path={'/login'} render={()=><LoginContainer />}/>
+                    <Route path={'/users'} render={() => <UsersContainer/>}/>
+                    <Route path={'/login'} render={() => <LoginContainer/>}/>
                     {/*<Route path={'*'} render={() => <h1 style={{textAlign: 'center'}}>404: PAGE NOT FOUND</h1>}/>*/}
                 </div>
 
@@ -104,16 +106,14 @@ const AppContainer = compose(
 )(App);
 
 
-type SocialNetworkAppProps = {
-
-}
+type SocialNetworkAppProps = {}
 
 const SocialNetworkApp = (props: SocialNetworkAppProps) => {
-return <BrowserRouter>
-    <Provider store={store}>
-        <AppContainer  />
-    </Provider>
-</BrowserRouter>
+    return <BrowserRouter>
+        <Provider store={store}>
+            <AppContainer/>
+        </Provider>
+    </BrowserRouter>
 }
 
 export default SocialNetworkApp
